da6a400e99afc68dda361a8ae7e416f63d53fc7f
Zaixu Cui <zaixucui@gmail.com>
2018-02-04 15:20:51 -0500

    delete

--- Energy_Activation_Relation/InitialDM1_TargetActivation/MediationAnalysis.m
+++
@@ -1,7 +0,0 @@
-
-Energy_Activation = load('/data/jux/BBL/projects/pncControlEnergy/results/InitialDM1_TargetActivation/Energy_Activation_Relationship/Energy_Activation.mat');
-Activation_abs = Energy_Activation.Activation_abs;
-Energy_New = Energy_Activation.Energy_New;
-corr(Activation_abs(:, 184), Energy_New(:, 184));
-
-
