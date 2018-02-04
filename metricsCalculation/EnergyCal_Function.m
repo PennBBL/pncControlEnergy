3f045cc10036b9c2879b72a44dfb6ea44f0299b7
Zaixu Cui <zaixucui@gmail.com>
2018-02-04 15:20:09 -0500

    delete

--- metricsCalculation/EnergyCal_Function.m
+++
@@ -1,9 +0,0 @@
-
-function [X_Opt_Trajectory, X_Opt_Final, U_Opt_Trajectory, Energy, n_err] = EnergyCal_Function(ConnPath, scale_factor, T, xc, x0, xf, rho, ResultantFile)
-
-load(ConnPath);
-A = connectivity ./ scale_factor;
-A = A - 2*max(real(eig(A)))*eye(size(A)); % scale to be stable, makes calculation more tractable
-
-[X_Opt_Trajectory, X_Opt_Final, U_Opt_Trajectory, Energy, n_err] = optim_fun(A, T, diag(xc), x0, xf, rho);
-save(ResultantFile, 'X_Opt_Trajectory', 'X_Opt_Final', 'U_Opt_Trajectory', 'Energy', 'n_err', 'xc', 'xf');
