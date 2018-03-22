321b5ad39b0a5d9275da6d40b6525d4ac97f06a0
Zaixu Cui <zaixucui@gmail.com>
2018-03-22 15:10:35 -0400

    delete

--- 4th_Energy_Calculation/EnergyCal_Function.m
+++
@@ -1,9 +0,0 @@
-
-function [X_Opt_Trajectory, X_Opt_Final, U_Opt_Trajectory, Energy, n_err] = EnergyCal_Function(ConnPath, T, xc, x0, xf, rho, ResultantFile)
-
-load(ConnPath);
-A = connectivity ./ svds(connectivity, 1);
-A = A - eye(size(A));
-
-[X_Opt_Trajectory, X_Opt_Final, U_Opt_Trajectory, Energy, n_err] = optim_fun(A, T, diag(xc), x0, xf, rho);
-save(ResultantFile, 'X_Opt_Trajectory', 'X_Opt_Final', 'U_Opt_Trajectory', 'Energy', 'n_err', 'xc', 'xf');
